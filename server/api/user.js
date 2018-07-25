3f01022fb8c10ddb96368bf1da13291b4363bcea
xianxin <383557980@qq.com>
2018-07-26 00:20:31 +0800

    '完善注册'

--- server/api/user.js
+++ server/api/user.js
@@ -1,13 +1,22 @@
 const KoaRouter = require('koa-router');
 const Router = new KoaRouter();
 
+const UserModel = require('../model/User');
+
 Router.get('/',async(ctx)=>{
     ctx.body ='user'
 })
 
 Router.post('/register',async(ctx)=>{
-    console.log(ctx.request.body);
-    ctx.body =ctx.request.body
+    UserModel.create({
+        userName:ctx.request.body.userName,passWord:ctx.request.body.passWord
+    }).then(doc=>{
+        console.log(doc);
+        ctx.body =ctx.request.body
+    }).catch(error=>{
+        console.log(error);
+    })
+   
 })
 
 module.exports = Router;
